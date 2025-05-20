c7dcd32c47d55b74bc8033872dc822e9be1e16ac
iaremchuk <ya.olya24@gmail.com>
2025-05-20 15:42:12 +0100

    Added remaining elemnts to OfferDetails page

--- src/containers/offer-details-container/OfferDetailsContainer.jsx
+++ src/containers/offer-details-container/OfferDetailsContainer.jsx
@@ -2,9 +2,9 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import { styles } from '~/containers/offer-details-container/OfferDetailsContainer.styles'
 
-const OfferDetailsContainer = ({ title, children }) => {
+const OfferDetailsContainer = ({ title, children, sx = {} }) => {
   return (
-    <Box sx={styles.container}>
+    <Box sx={{ ...styles.container, ...sx }}>
       <Typography sx={styles.title}>{title}</Typography>
       {children}
     </Box>
